7bebbf51be1fd78a77c25e4969fa6bc8ec4291b0
Takafumi Tamura <taktamur@gmail.com>
2013-02-16 14:16:01 +0900

    add camera.

---
+++ MegSampleApp/src/jp/co/olympus/megsampleapp/CameraActivity.java
@@ -0,0 +1,12 @@
+package jp.co.olympus.megsampleapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CameraActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
