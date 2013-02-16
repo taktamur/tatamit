b3d8f75bf98b6a16e9af21f58fa65a2b5144fb59
Takafumi Tamura <taktamur@gmail.com>
2013-02-16 15:00:24 +0900

    add trans button, and sample date string print.

--- MegSampleApp/gen/jp/co/olympus/megsampleapp/R.java
+++ MegSampleApp/gen/jp/co/olympus/megsampleapp/R.java
@@ -14,16 +14,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button_scan=0x7f070006;
-        public static final int button_sensor_stop=0x7f070008;
-        public static final int editText1=0x7f070000;
-        public static final int listview=0x7f070001;
-        public static final int menu_settings=0x7f070009;
-        public static final int new_devices=0x7f070005;
-        public static final int paired_devices=0x7f070003;
-        public static final int sensorView=0x7f070007;
-        public static final int title_new_devices=0x7f070004;
-        public static final int title_paired_devices=0x7f070002;
+        public static final int button_scan=0x7f070007;
+        public static final int button_sensor_stop=0x7f070009;
+        public static final int listview=0x7f070002;
+        public static final int menu_settings=0x7f07000a;
+        public static final int message=0x7f070000;
+        public static final int new_devices=0x7f070006;
+        public static final int paired_devices=0x7f070004;
+        public static final int sensorView=0x7f070008;
+        public static final int title_new_devices=0x7f070005;
+        public static final int title_paired_devices=0x7f070003;
+        public static final int trans=0x7f070001;
     }
     public static final class layout {
         public static final int activity_camera=0x7f030000;
--- MegSampleApp/src/jp/co/olympus/megsampleapp/CameraActivity.java
+++ MegSampleApp/src/jp/co/olympus/megsampleapp/CameraActivity.java
@@ -1,12 +1,32 @@
 package jp.co.olympus.megsampleapp;
 
+import java.util.Date;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.*;
+import android.widget.*;
 
 public class CameraActivity extends Activity {
-    @Override
+	private Button transButton ;
+	private TextView message;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        transButton = (Button)this.findViewById(R.id.trans);
+        message = (TextView)this.findViewById(R.id.message);
+        
+        // ボタンのトリガー設定(後で1秒おきに変更) 
+        // TODO
+        transButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				message.setText((new Date()).toString());
+			}
+		});
     }
 }
